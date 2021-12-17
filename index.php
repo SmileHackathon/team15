9ff7b071afdb2379b463c9ed59c8e42cdc4479b1
YoshinoTakazawa <yochiyochiperry2000@gmail.com>
2021-12-18 05:08:45 +0900

    ガチャを作るところのcssの作成

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 
 <head>
     <meta charset="UTF-8">
+    <link rel=" stylesheet" href="./css/style_index.css">
+    <link rel=" stylesheet" href="./css/reset.css">
     <title>HTML+CSS トレーニング</title>
 </head>
 
 <body>
-    <button onclick="location.href='./login.php'">ガチャを作る！</button>
+    <!-- ヘッダー-->
+    <div class="sample">
+        <img class="header_logo" src="logo.png" style="float: left;">
+    </div>
+
+    <!-- ガチャを作るボタン-->
+    <button class="make_button" onclick="location.href='./login.php'">
+        <span class="button_text">ガチャを作る！</span>
+    </button>
 </body>
