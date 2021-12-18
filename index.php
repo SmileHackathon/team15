4762a490f9a27441f86502e84f0385da3ee13872
mrs1669 <mrs1669.develop@gmail.com>
2021-12-18 12:25:34 +0900

    update index.html

--- index.php
+++ index.php
@@ -9,13 +9,15 @@
 </head>
 
 <body>
+    <button onclick="location.href='./login.php'">ガチャを作る！</button>
+</body>
     <!-- ヘッダー-->
     <div class="sample">
         <img class="header_logo" src="logo.png" style="float: left;">
     </div>
-
     <!-- ガチャを作るボタン-->
     <button class="make_button" onclick="location.href='./login.php'">
         <span class="button_text">ガチャを作る！</span>
     </button>
 </body>
+</html> 
